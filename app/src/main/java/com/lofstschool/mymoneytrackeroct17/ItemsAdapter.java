84af9d1d6a018d842b89b5ebc7092e41d999bc0e
Nikolay Kuznetsov <nickblacksmith777@gmail.com>
2017-11-03 20:07:05 +0300

    New Activity

---
+++ app/src/main/java/com/lofstschool/mymoneytrackeroct17/ItemsAdapter.java
@@ -0,0 +1,80 @@
+package com.lofstschool.mymoneytrackeroct17;
+
+import android.support.v7.view.menu.MenuView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemViewHolder> {
+
+    private List <Item> Items = new ArrayList<>();
+
+    public ItemsAdapter() {
+        Items.add(new Item("Молоко", 35));
+        Items.add(new Item("Зубная паста", 1500));
+        Items.add(new Item("Мясо", 200));
+        Items.add(new Item("Телек", 1000));
+        Items.add(new Item("ТВ приставка для телевизора", 550));
+        Items.add(new Item("Молоко", 35));
+        Items.add(new Item("Зубная паста", 1500));
+        Items.add(new Item("Мясо", 200));
+        Items.add(new Item("Телек", 1000));
+        Items.add(new Item("ТВ приставка для телевизора", 550));
+        Items.add(new Item("Молоко", 35));
+        Items.add(new Item("Зубная паста", 1500));
+        Items.add(new Item("Мясо", 200));
+        Items.add(new Item("Телек", 1000));
+        Items.add(new Item("ТВ приставка для телевизора", 550));
+        Items.add(new Item("Молоко", 35));
+        Items.add(new Item("Зубная паста", 1500));
+        Items.add(new Item("Мясо", 200));
+        Items.add(new Item("Телек", 1000));
+        Items.add(new Item("ТВ приставка для телевизора", 550));
+
+    }
+
+    @Override
+    public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent,
+        false );
+        return new ItemViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ItemViewHolder holder, int position) {
+        holder.price.setText(String.valueOf(Items.get(position).getPrice()));
+        holder.name.setText(Items.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+
+        return Items.size();
+    }
+
+
+
+
+    static class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        private TextView name;
+        private    TextView price;
+
+        ItemViewHolder(View ItemView) {
+            super(ItemView);
+            name = itemView.findViewById(R.id.item_name);
+            price = itemView.findViewById(R.id.item_price);
+        }
+        void bind (Item item){
+            name.setText(item.getName());
+            price.setText(String.valueOf(item.getPrice()));
+        }
+    }
+
+}
